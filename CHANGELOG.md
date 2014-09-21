06076fe392580485c2234687441e504aa80109ff
Jason Dobry <jason.dobry@gmail.com>
2014-09-20 21:05:13 -0600

    Stable Version 0.2.0.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+##### 0.2.0 - 2 September 2014
+
+###### Backwards compatible API changes
+- Added deserialize and serialize.
+
 ##### 0.1.0 - 17 September 2014
 
-Initial release
+- Initial release
--- dist/js-data-http.js
+++ dist/js-data-http.js
@@ -1,7 +1,7 @@
 /**
 * @author Jason Dobry <jason.dobry@gmail.com>
 * @file js-data-http.js
-* @version 0.1.0 - Homepage <http://www.js-data.iojs-data-http/>
+* @version 0.2.0 - Homepage <http://www.js-data.iojs-data-http/>
 * @copyright (c) 2014 Jason Dobry 
 * @license MIT <https://github.com/js-data/js-data-http/blob/master/LICENSE>
 *
@@ -1304,141 +1304,65 @@ function Defaults() {
 
 }
 
-/**
- * @doc property
- * @id DSHttpAdapter.properties:defaults.queryTransform
- * @name defaults.queryTransform
- * @description
- * Transform the js-data query to something your server understands. You might just do this on the server instead.
- *
- * ## Example:
- * ```js
- * DSHttpAdapter.defaults.queryTransform = function (resourceName, params) {
- *   if (params && params.userId) {
- *     params.user_id = params.userId;
- *     delete params.userId;
- *   }
- *   return params;
- * };
- * ```
- *
- * @param {string} resourceName The name of the resource.
- * @param {object} params Params that will be passed to `http`.
- * @returns {*} By default just returns `params` as-is.
- */
-Defaults.prototype.queryTransform = function (resourceName, params) {
+var defaultsPrototype = Defaults.prototype;
+
+defaultsPrototype.queryTransform = function (resourceName, params) {
   return params;
 };
 
-/**
- * @doc property
- * @id DSHttpAdapter.properties:defaults.httpConfig
- * @name defaults.httpConfig
- * @description
- * Default http configuration options used whenever `DSHttpAdapter` uses `http`.
- *
- * ## Example:
- * ```js
- * var dsHttpAdapter = new DSHttpAdapter({
- *   httpConfig: {
- *     headers: {
- *       Authorization: 'Basic YmVlcDpib29w'
- *     },
- *     timeout: 20000
- *   });
- * });
- * ```
- */
-Defaults.prototype.httpConfig = {};
+defaultsPrototype.httpConfig = {};
 
-/**
- * @doc constructor
- * @id DSHttpAdapter
- * @name DSHttpAdapter
- * @description
- * Default adapter used by js-data. This adapter uses AJAX and JSON to send/retrieve data to/from a server.
- * Developers may provide custom adapters that implement the adapter interface.
- */
+defaultsPrototype.log = console ? console.log : function () {
+};
+
+defaultsPrototype.deserialize = function (resourceName, data) {
+  return data ? ('data' in data ? data.data : data) : data;
+};
+
+defaultsPrototype.serialize = function (resourceName, data) {
+  return data;
+};
+
 function DSHttpAdapter(options) {
-  /**
-   * @doc property
-   * @id DSHttpAdapter.properties:defaults
-   * @name defaults
-   * @description
-   * Reference to [DSHttpAdapter.defaults](/documentation/api/api/DSHttpAdapter.properties:defaults).
-   */
   this.defaults = new Defaults();
   deepMixIn(this.defaults, options);
 }
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:HTTP
- * @name HTTP
- * @description
- * A wrapper for `http()`.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.HTTP(config)
- * ```
- *
- * @param {object} config Configuration object.
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.HTTP = function (config) {
+var dsHttpAdapterPrototype = DSHttpAdapter.prototype;
+
+dsHttpAdapterPrototype.HTTP = function (config, resourceConfig) {
+  var _this = this;
   var start = new Date().getTime();
+  var deserialize;
+  if (config.deserialize) {
+    deserialize = config.deserialize;
+    delete config.deserialize;
+  } else {
+    deserialize = _this.defaults.deserialize;
+  }
+  resourceConfig = resourceConfig || {};
   config = deepMixIn(config, this.defaults.httpConfig);
   return http(config).then(function (data) {
-    console.log(data.config.method + ' request:' + data.config.url + ' Time taken: ' + (new Date().getTime() - start) + 'ms', arguments);
-    return data.data;
+    if (_this.defaults.log) {
+      var args = Array.prototype.slice.call(arguments);
+      args.unshift(data.config.method + ' request: ' + data.config.url + ' Time taken: ' + (new Date().getTime() - start) + 'ms');
+      _this.defaults.log.apply(_this.defaults.log, args);
+    }
+    return deserialize(resourceConfig.name, data);
   });
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:GET
- * @name GET
- * @description
- * A wrapper for `http.get()`.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.GET(url[, config])
- * ```
- *
- * @param {string} url The url of the request.
- * @param {object=} config Optional configuration.
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.GET = function (url, config) {
+dsHttpAdapterPrototype.GET = function (url, config, resourceConfig) {
   config = config || {};
   if (!('method' in config)) {
     config.method = 'get';
   }
   return this.HTTP(deepMixIn(config, {
     url: url
-  }));
+  }), resourceConfig);
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:POST
- * @name POST
- * @description
- * A wrapper for `http.post()`.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.POST(url[, attrs][, config])
- * ```
- *
- * @param {string} url The url of the request.
- * @param {object=} attrs Request payload.
- * @param {object=} config Optional configuration.
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.POST = function (url, attrs, config) {
+dsHttpAdapterPrototype.POST = function (url, attrs, config, resourceConfig) {
   config = config || {};
   if (!('method' in config)) {
     config.method = 'post';
@@ -1446,27 +1370,10 @@ DSHttpAdapter.prototype.POST = function (url, attrs, config) {
   return this.HTTP(deepMixIn(config, {
     url: url,
     data: attrs
-  }));
+  }), resourceConfig);
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:PUT
- * @name PUT
- * @description
- * A wrapper for `http.put()`.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.PUT(url[, attrs][, config])
- * ```
- *
- * @param {string} url The url of the request.
- * @param {object=} attrs Request payload.
- * @param {object=} config Optional configuration.
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.PUT = function (url, attrs, config) {
+dsHttpAdapterPrototype.PUT = function (url, attrs, config, resourceConfig) {
   config = config || {};
   if (!('method' in config)) {
     config.method = 'put';
@@ -1474,92 +1381,29 @@ DSHttpAdapter.prototype.PUT = function (url, attrs, config) {
   return this.HTTP(deepMixIn(config, {
     url: url,
     data: attrs || {}
-  }));
+  }), resourceConfig);
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:DEL
- * @name DEL
- * @description
- * A wrapper for `http.delete()`.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.DEL(url[, config])
- * ```
- *
- * @param {string} url The url of the request.
- * @param {object=} config Optional configuration.
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.DEL = function (url, config) {
+dsHttpAdapterPrototype.DEL = function (url, config, resourceConfig) {
   config = config || {};
   if (!('method' in config)) {
     config.method = 'delete';
   }
   return this.HTTP(deepMixIn(config, {
     url: url
-  }));
+  }), resourceConfig);
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:find
- * @name find
- * @description
- * Retrieve a single entity from the server.
- *
- * Makes a `GET` request.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.find(resourceConfig, id[, options])
- * ```
- *
- * @param {object} resourceConfig DS resource definition object:
- * @param {string|number} id Primary key of the entity to update.
- * @param {object=} options Optional configuration. Also passed along to `http([config])`. Properties:
- *
- * - `{string=}` - `baseUrl` - Override the default base url.
- * - `{string=}` - `endpoint` - Override the default endpoint.
- * - `{object=}` - `params` - Additional query string parameters to add to the url.
- *
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.find = function (resourceConfig, id, options) {
+dsHttpAdapterPrototype.find = function (resourceConfig, id, options) {
   options = options || {};
   return this.GET(
     makePath(options.baseUrl || resourceConfig.baseUrl, resourceConfig.getEndpoint(id, options), id),
-    options
+    options,
+    resourceConfig
   );
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:findAll
- * @name findAll
- * @description
- * Retrieve a collection of entities from the server.
- *
- * Makes a `GET` request.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.findAll(resourceConfig[, params][, options])
- * ```
- *
- * @param {object} resourceConfig DS resource definition object:
- * @param {object=} params Search query parameters. See the [query guide](/documentation/guide/queries/index).
- * @param {object=} options Optional configuration. Also passed along to `http([config])`. Properties:
- *
- * - `{string=}` - `baseUrl` - Override the default base url.
- * - `{string=}` - `endpoint` - Override the default endpoint.
- * - `{object=}` - `params` - Additional query string parameters to add to the url.
- *
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.findAll = function (resourceConfig, params, options) {
+dsHttpAdapterPrototype.findAll = function (resourceConfig, params, options) {
   options = options || {};
   options.params = options.params || {};
   if (params) {
@@ -1568,103 +1412,35 @@ DSHttpAdapter.prototype.findAll = function (resourceConfig, params, options) {
   }
   return this.GET(
     makePath(options.baseUrl || resourceConfig.baseUrl, resourceConfig.getEndpoint(null, options)),
-    options
+    options,
+    resourceConfig
   );
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:create
- * @name create
- * @description
- * Create a new entity on the server.
- *
- * Makes a `POST` request.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.create(resourceConfig, attrs[, options])
- * ```
- *
- * @param {object} resourceConfig DS resource definition object:
- * @param {object} attrs The attribute payload.
- * @param {object=} options Optional configuration. Also passed along to `http([config])`. Properties:
- *
- * - `{string=}` - `baseUrl` - Override the default base url.
- * - `{string=}` - `endpoint` - Override the default endpoint.
- * - `{object=}` - `params` - Additional query string parameters to add to the url.
- *
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.create = function (resourceConfig, attrs, options) {
+dsHttpAdapterPrototype.create = function (resourceConfig, attrs, options) {
+  var _this = this;
   options = options || {};
   return this.POST(
     makePath(options.baseUrl || resourceConfig.baseUrl, resourceConfig.getEndpoint(attrs, options)),
-    attrs,
-    options
+    options.serialize ? options.serialize(resourceConfig.name, attrs) : _this.defaults.serialize(resourceConfig.name, attrs),
+    options,
+    resourceConfig
   );
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:update
- * @name update
- * @description
- * Update an entity on the server.
- *
- * Makes a `PUT` request.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.update(resourceConfig, id, attrs[, options])
- * ```
- *
- * @param {object} resourceConfig DS resource definition object:
- * @param {string|number} id Primary key of the entity to update.
- * @param {object} attrs The attribute payload.
- * @param {object=} options Optional configuration. Also passed along to `http([config])`. Properties:
- *
- * - `{string=}` - `baseUrl` - Override the default base url.
- * - `{string=}` - `endpoint` - Override the default endpoint.
- * - `{object=}` - `params` - Additional query string parameters to add to the url.
- *
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.update = function (resourceConfig, id, attrs, options) {
+dsHttpAdapterPrototype.update = function (resourceConfig, id, attrs, options) {
+  var _this = this;
   options = options || {};
   return this.PUT(
     makePath(options.baseUrl || resourceConfig.baseUrl, resourceConfig.getEndpoint(id, options), id),
-    attrs,
-    options
+    options.serialize ? options.serialize(resourceConfig.name, attrs) : _this.defaults.serialize(resourceConfig.name, attrs),
+    options,
+    resourceConfig
   );
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:updateAll
- * @name updateAll
- * @description
- * Update a collection of entities on the server.
- *
- * Makes a `PUT` request.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.updateAll(resourceConfig, attrs[, params][, options])
- * ```
- *
- * @param {object} resourceConfig DS resource definition object:
- * @param {object} attrs The attribute payload.
- * @param {object=} params Search query parameters. See the [query guide](/documentation/guide/queries/index).
- * @param {object=} options Optional configuration. Also passed along to `http([config])`. Properties:
- *
- * - `{string=}` - `baseUrl` - Override the default base url.
- * - `{string=}` - `endpoint` - Override the default endpoint.
- * - `{object=}` - `params` - Additional query string parameters to add to the url.
- *
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.updateAll = function (resourceConfig, attrs, params, options) {
+dsHttpAdapterPrototype.updateAll = function (resourceConfig, attrs, params, options) {
+  var _this = this;
   options = options || {};
   options.params = options.params || {};
   if (params) {
@@ -1673,68 +1449,22 @@ DSHttpAdapter.prototype.updateAll = function (resourceConfig, attrs, params, opt
   }
   return this.PUT(
     makePath(options.baseUrl || resourceConfig.baseUrl, resourceConfig.getEndpoint(null, options)),
-    attrs,
-    options
+    options.serialize ? options.serialize(resourceConfig.name, attrs) : _this.defaults.serialize(resourceConfig.name, attrs),
+    options,
+    resourceConfig
   );
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:destroy
- * @name destroy
- * @description
- * Delete an entity on the server.
- *
- * Makes a `DELETE` request.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.destroy(resourceConfig, id[, options)
- * ```
- *
- * @param {object} resourceConfig DS resource definition object:
- * @param {string|number} id Primary key of the entity to update.
- * @param {object=} options Optional configuration. Also passed along to `http([config])`. Properties:
- *
- * - `{string=}` - `baseUrl` - Override the default base url.
- * - `{string=}` - `endpoint` - Override the default endpoint.
- * - `{object=}` - `params` - Additional query string parameters to add to the url.
- *
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.destroy = function (resourceConfig, id, options) {
+dsHttpAdapterPrototype.destroy = function (resourceConfig, id, options) {
   options = options || {};
   return this.DEL(
     makePath(options.baseUrl || resourceConfig.baseUrl, resourceConfig.getEndpoint(id, options), id),
-    options
+    options,
+    resourceConfig
   );
 };
 
-/**
- * @doc method
- * @id DSHttpAdapter.methods:destroyAll
- * @name destroyAll
- * @description
- * Delete a collection of entities on the server.
- *
- * Makes `DELETE` request.
- *
- * ## Signature:
- * ```js
- * DSHttpAdapter.destroyAll(resourceConfig[, params][, options])
- * ```
- *
- * @param {object} resourceConfig DS resource definition object:
- * @param {object=} params Search query parameters. See the [query guide](/documentation/guide/queries/index).
- * @param {object=} options Optional configuration. Also passed along to `http([config])`. Properties:
- *
- * - `{string=}` - `baseUrl` - Override the default base url.
- * - `{string=}` - `endpoint` - Override the default endpoint.
- * - `{object=}` - `params` - Additional query string parameters to add to the url.
- *
- * @returns {Promise} Promise.
- */
-DSHttpAdapter.prototype.destroyAll = function (resourceConfig, params, options) {
+dsHttpAdapterPrototype.destroyAll = function (resourceConfig, params, options) {
   options = options || {};
   options.params = options.params || {};
   if (params) {
@@ -1743,7 +1473,8 @@ DSHttpAdapter.prototype.destroyAll = function (resourceConfig, params, options) 
   }
   return this.DEL(
     makePath(options.baseUrl || resourceConfig.baseUrl, resourceConfig.getEndpoint(null, options)),
-    options
+    options,
+    resourceConfig
   );
 };
 
